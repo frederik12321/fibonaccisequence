7cdf6f441857dffd4d67ec6554dc4c243fa2091e
Frederik Beliën <frederikbelien@MacBook-Pro-van-Frederik.local>
2016-05-17 23:11:09 +0200

    Initial commit

---
+++ fibonaccisequence.py
@@ -0,0 +1,14 @@
+#this little program creates a Fibonacci Sequence and asks you which number you need
+
+fibo = [0,1]
+def fibor(fibo):
+    for each in range(2,1000):
+        fibo.append(fibo[each - 2]+fibo[each - 1])
+
+fibor(fibo)
+
+usernumber = int(input("Which Fibonacci Sequence number do you need? "))
+
+print(fibo[usernumber])
+
+
